0e2a89e109ad5b66fe9a290ce13651a370743940
Yoonsuck Choe <choe@tamu.edu>
2024-10-10 09:54:14 -0500

    plotting - needs more work

--- hopfield.m
+++ hopfield.m
@@ -22,13 +22,13 @@
 #
 # 1. Noisy input
 #
-# g = []; for i=1:10; g =[g; vec(dogfilter(rand(20,20),17))'>0]; end; inp=g(5,:).*(rand(1,20*20)<0.6); imagesc(reshape(inp,20,20)); inp=inp*2-1; g=g*2-1; [X,w] = hopfield(inp',g,100,5);
+# g = []; for i=1:10; g =[g; vec(dogfilter(rand(20,20),17))'>0]; end; inp=g(5,:).*(rand(1,20*20)<0.6); figure(i+2); imagesc(reshape(inp,20,20)); inp=inp*2-1; g=g*2-1; [X,w] = hopfield(inp',g,100,5);
 #
-# g = []; for i=1:10; g =[g; vec(dogfilter(rand(20,20),17))'>0]; end; inp=(g(5,:)*2-1).*((rand(1,20*20)<0.2)*2-1); imagesc(reshape(inp,20,20)); g=-(g*2-1); [X,w] = hopfield(inp',g,100,5);
+# g = []; for i=1:10; g =[g; vec(dogfilter(rand(20,20),17))'>0]; end; inp=(g(5,:)*2-1).*((rand(1,20*20)<0.2)*2-1); figure(i+2); imagesc(reshape(inp,20,20)); g=-(g*2-1); [X,w] = hopfield(inp',g,100,5);
 #
 # 2. Partial input
 #
-# g = []; for i=1:10; g =[g; vec(dogfilter(rand(20,20),17))'>0]; end; inp=reshape(g(5,:),20,20); inp(1:10,:)=zeros(10,20); inp=vec(inp)'; imagesc(reshape(inp,20,20)); inp=inp*2-1; g=g*2-1; [X,w] = hopfield(inp',g,100,5);
+# g = []; for i=1:10; g =[g; vec(dogfilter(rand(20,20),17))'>0]; end; inp=reshape(g(5,:),20,20); inp(1:10,:)=zeros(10,20); inp=vec(inp)'; figure(i+2); imagesc(reshape(inp,20,20)); inp=inp*2-1; g=g*2-1; [X,w] = hopfield(inp',g,100,5);
 #
 # Author: Yoonsuck Choe
 # http://faculty.cs.tamu.edu/choe
@@ -91,7 +91,7 @@ for i=1:num_iter
 
 	#--------------------
 	# 3.3 Plot results: you may need to tinker with this a bit.
-	#	
+	# - this was supposed to show an animation. Will need a better method.
 	#--------------------
         loc=find(x>=0); locx = floor(loc/m); locy = rem(loc,m);
         loc0=find(x0>=0); locx0 = floor(loc0/m); locy0 = rem(loc0,m);
